247f55e64561cc84d1e561bb672b0280853bc8bb
Sofya Asinskaya <sofya.asinskaya@student.ehu.lt>
2024-05-05 16:16:38 +0300

    feat(hometask2):add

--- src/forms/script.js
+++ src/forms/script.js
@@ -1,1 +1,26 @@
-// Your solution here
+const form = document.querySelector('.form form');
+const emailInput = document.getElementById('field1');
+const passwordInput = document.getElementById('field2');
+const repeatInput = document.getElementById('field3');
+const signUpButton = document.getElementById('sub');
+
+signUpButton.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  const inputs = [emailInput, passwordInput, repeatInput];
+  let hasError = false;
+  
+  inputs.forEach(input => {
+    if (input.value.trim() === '') {
+        input.style.borderColor = '#9e1b32';
+      hasError = true;
+    } else {
+        input.style.borderColor = '#EBEAED';
+    }
+  });
+  
+  if (hasError) {
+    alert('Failure');
+  } else {
+    alert('Success!');
+  }
+});
